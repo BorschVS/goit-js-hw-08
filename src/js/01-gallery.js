38dafa9d04d989555c6b65e7e949a4661ec4806d
BorschVS <borschvadym@gmail.com>
2023-10-21 22:23:40 +0300

    solution for 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const itemsMarkup = createListItemsMarkup(galleryItems);
+
+gallery.innerHTML = itemsMarkup;
+
+document.addEventListener('DOMContentLoaded', createNewSimpleLigthBox);
+
+function createNewSimpleLigthBox() {
+  new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    captionsData: 'alt',
+    navText: ['‹', '›'],
+    fadeSpeed: 250,
+  });
+}
+
+function createListItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+    </li>
+    `;
+    })
+    .join('');
+}
