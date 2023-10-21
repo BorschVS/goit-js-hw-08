6e368ca7edfe3003a226f37ab06c2bf6d7b23191
BorschVS <borschvadym@gmail.com>
2023-10-22 00:09:00 +0300

    add npm vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', onTimeUpdate);
+
+function onTimeUpdate() {
+  player.getCurrentTime().then(function (seconds) {});
+}
+// const json = JSON.stringify(user);
+
+// localStorage.setItem("user", json);
+
+// const savedData = localStorage.getItem("user");
+// console.log("savedData:", savedData);
+
+// const parsedData = JSON.parse(savedData);
+// console.log("parsedData", parsedData);
+
+// export default json;
